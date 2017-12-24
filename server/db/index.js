49f065b6235cca96ffc7d42272f6a4e7fb03cb2f
Kaitlyn Menghini <kaitlyn.menghini@gmail.com>
2017-12-24 00:34:37 -0800

    fix spec and create database connection

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,20 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-
+module.exports = {
+  databaseConnection: function(queryString, callback) {
+    var connection = mysql.createConnection({
+      user: 'root',
+      database: 'chat'
+    });
+    connection.connect();
+    connection.query(queryString, function(error, rows) {
+      if (error) {
+        return error;
+      }
+      console.log(rows);
+      callback(rows);
+    });
+    connection.end();
+  }
+};
